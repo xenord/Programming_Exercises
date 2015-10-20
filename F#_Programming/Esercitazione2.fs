fc46eaf1470b190060224694a184af6edd091adc
Francesco <francesco.benetello921@gmail.com>
2015-10-20 15:24:35 +0200

    Upload description

--- F#_Programming/Esercitazione2.fs
+++ F#_Programming/Esercitazione2.fs
@@ -1,5 +1,151 @@
 module Esercitazione2
 
+(*
+Alcune note utili:
+
+Il terzo esercizio è collegato alla scorsa esercitazione, si consiglia di leggere la scorsa esercitazione (se già non l'avete fatto)
+
+
+Convenzioni
+
+Le seguenti convenzioni sono da ritenersi vincolanti affinché la vostra soluzione venga considerata corretta!
+
+La durata di un ciclo lunare è pari ad ESATTAMENTE 29 giorni.
+
+Ipotizziate che il 1 gennaio del 2000 fosse l'inizio di un ciclo lunare (ci fosse quindi la Luna Nuova e il giorno corrispondente del ciclo lunare fosse 0).
+
+
+Le fasi lunari sono definite come segue:
+
+
+(Fase) (Intervallo Giorni) (Codice)
+Luna Nuova 0 - 3	 LN
+Luna Crescente 4 - 6 LR
+Primo Quarto 7 - 10	PQ
+Gibbosa Crescente 11 - 14 GR
+Luna Piena 15 - 18 LP
+Gibbosa Calante 19 - 21 GA
+Ultimo Quarto 22 - 25 UQ
+Luna Calante 26 - 28 LA
+
+Una luna piena è considerata parte di un mese se il suo 15° giorno di ciclo fa parte del mese in questione
+La durata dei mesi è quella del calendario Gregoriano, COMPRESI GLI ANNI BISESTILI .
+
+
+Un anno si definisce bisestile se il suo numero è divisibile per 4, con l'eccezione degli anni secolari (quelli divisibili per 100) che non sono divisibili per 400.
+
+L'anno 2000 è bisestile perché è un anno secolare ma è divisibile per 400.
+L'anno 1900 non è bisestile perché è secolare ma non è divisibile per 400.
+
+L'anno 1988 è bisestile perché è divisibile per 4.
+
+L'anno 1989 non è bisestile perchè non è divisibile per 4.
+
+
+Una data gg/mm/aaaa è definita come segue:
+
+Il numero	corrisponde a      	con le limitazioni
+gg	giorno del mese	da 1 a 31
+mm	mese dell'anno
+da 1 a 12
+aaaa	cifre dell'anno
+da 0 in avanti
+
+
+Esercizio 1:
+
+Date due date gg/mm/aaaa si richiede di scrivere una funzione che:
+presi in input i numeri che compongono le due date, restituisca come output la differenza in giorni tra le due date, considerando anche gli anni bisestili.
+
+
+data_difference 10 1 1989 15 12 1988 ==> 26
+
+data_difference 30 9 2015 8 10 2015 ==> -8
+
+data_difference 8 10 2015 8 10 2015 ==> 0
+
+data_difference 6 1 2001 1 1 2000 ==> 371
+
+data_difference 6 1 2005 1 1 2000 ==> 1832
+
+data_difference 28 2 2012 1 3 2012 ==> -2
+
+
+Firma:
+
+data_difference: int -> int -> int -> int -> int -> int -> int
+
+
+
+Esercizio 2:
+
+Dato un numero, si richiede di scrivere una funzione che:
+preso in input un numero, restituisca come output il numero con le cifre invertite.
+
+NON USATE la conversione in Stringhe
+
+
+Esempio:
+
+
+reverse_number 12345 ==> 54321
+
+reverse_number 7  ==> 7
+
+reverse_number 100 ==> 1
+
+reverse_number 101 ==> 101
+
+reverse_number 1203 ==> 3021
+
+Firma:
+
+reverse_number: int -> int
+
+
+
+Esercizio 3:
+
+Dato il termine Luna Blu, che indica la seconda luna piena di un mese, si richiede di scrivere una funzione che:
+
+preso in input un anno, restituisca come output il numero del primo mese dell'anno che contenga una luna blu, in caso nessun mese ne abbia si deve lanciare un'eccezione failwith "NON TROVATO", considerando anche gli anni bisestili.
+
+
+Esempio:
+
+blue_moon 1988 ==> NON TROVATO
+
+blue_moon 2015 ==> NON TROVATO
+
+blue_moon 2020 ==> NON TROVATO
+
+blue_moon 2016 ==> 1
+
+blue_moon 1989 ==> 3
+
+blue_moon 2004 ==> 4
+
+blue_moon 2009 ==> 5
+
+blue_moon 2019 ==> 6
+
+blue_moon 1992 ==> 7
+
+blue_moon 2002 ==> 8
+
+blue_moon 2017 ==> 9
+
+blue_moon 1985 ==> 10
+
+blue_moon 2000 ==> 11
+
+blue_moon 2005 ==> 12
+
+Firma:
+
+blue_moon: int -> int
+*)
+
 let NomeCognome : string = "Francesco Benetello"
 
 
